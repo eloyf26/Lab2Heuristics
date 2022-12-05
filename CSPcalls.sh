ffcaf9416aa53524db858379affbca63d6650dd8
eloyfernandez <eloyfernandez@noahmed.com>
2022-12-05 08:00:47 -0800

    updated test files

--- CSPcalls.sh
+++ CSPcalls.sh
@@ -1,7 +1,6 @@
-python CSPBusSeats.py ./InputFiles/StudentsHighIds.txt
-python CSPBusSeats.py ./InputFiles/StudentsDisabled.txt
-python CSPBusSeats.py ./InputFiles/Students3TroubleSiblings.txt
-python CSPBusSeats.py ./InputFiles/Students2TroubleSiblings1RedMob.txt
-python CSPBusSeats.py ./InputFiles/Students2TroubleSiblings1normal.txt
-python CSPBusSeats.py ./InputFiles/Students2RedMobSiblings.txt
-python CSPBusSeats.py ./InputFiles/Students16Firstyear.txt
+python CSPBusSeats.py ./InputOutputFiles/StudentsHighIds.txt
+python CSPBusSeats.py ./InputOutputFiles/StudentsDisabled.txt
+python CSPBusSeats.py ./InputOutputFiles/Students3TroubleSiblings.txt
+python CSPBusSeats.py ./InputOutputFiles/Students2TroubleSiblings1RedMob.txt
+python CSPBusSeats.py ./InputOutputFiles/Students2TroubleSiblings1normal.txt
+python CSPBusSeats.py ./InputOutputFiles/Students2RedMobSiblings.txt
